624c23f50eeeea69f4f82dec7d9be62f659bba1b
Esaú García Sánchez-Torija <egarcia@halfbrick.com>
2025-04-02 10:44:40 +0200

    Review error and fix indentation

--- Assets/Public/GooglePlayGames/com.google.play.games/Editor/GPGSProjectSettings.cs
+++ Assets/Public/GooglePlayGames/com.google.play.games/Editor/GPGSProjectSettings.cs
@@ -16,116 +16,86 @@
 
 // Keep this file even on unsupported configurations.
 
-namespace GooglePlayGames.Editor
-{
-    using System.Collections.Generic;
-    using System.IO;
-#if UNITY_2017_3_OR_NEWER
-    using UnityEngine.Networking;
-#else
-    using UnityEngine;
+using System.Collections.Generic;
+using System.IO;
 
+#if UNITY_2017_3_OR_NEWER
+using UnityEngine.Networking;
+#else
+using UnityEngine;
 #endif
 
-    public class GPGSProjectSettings
-    {
-        private static GPGSProjectSettings sInstance = null;
+namespace GooglePlayGames.Editor {
 
-        public static GPGSProjectSettings Instance
-        {
-            get
-            {
-                if (sInstance == null)
-                {
-                    sInstance = new GPGSProjectSettings();
-                }
+    public class GPGSProjectSettings {
 
-                return sInstance;
-            }
+        private static GPGSProjectSettings s_instance = null;
+
+        public static GPGSProjectSettings Instance {
+            get => s_instance ??= new GPGSProjectSettings();
         }
 
-        private bool mDirty = false;
-        private readonly string mFile;
-        private Dictionary<string, string> mDict = new Dictionary<string, string>();
+        private bool m_dirty = false;
+        private readonly string m_file;
+        private Dictionary<string, string> m_dict = new();
 
         private GPGSProjectSettings()
         {
-            mFile = GPGSUtil.SlashesToPlatformSeparator("ProjectSettings/GooglePlayGameSettings.txt");
+            m_file = GPGSUtil.SlashesToPlatformSeparator("ProjectSettings/GooglePlayGameSettings.txt");
 
-            StreamReader rd = null;
-
-            // read the settings file, this list is all the locations it can be in order of precedence.
-            string[] fileLocations =
-            {
-                mFile,
+            var files = new[] {
+                m_file,
                 GPGSUtil.SlashesToPlatformSeparator(Path.Combine(GPGSUtil.RootPath, "Editor/projsettings.txt")),
                 GPGSUtil.SlashesToPlatformSeparator("Assets/Editor/projsettings.txt")
             };
 
-            foreach (string f in fileLocations)
-            {
-                if (File.Exists(f))
-                {
-                    // assign the reader and break out of the loop
-                    rd = new StreamReader(f);
+            StreamReader rd = null;
+            foreach (var it in files) {
+                if (File.Exists(it)) {
+                    rd = new StreamReader(it);
                     break;
                 }
             }
+            if (rd == null) return;
 
-            if (rd != null)
-            {
-                while (!rd.EndOfStream)
-                {
-                    string line = rd.ReadLine();
-                    if (line == null || line.Trim().Length == 0)
-                    {
-                        break;
-                    }
-
+            using (rd) {
+                while (!rd.EndOfStream) {
+                    var line = rd.ReadLine();
+                    if (line == null) break;
                     line = line.Trim();
-                    string[] p = line.Split(new char[] {'='}, 2);
-                    if (p.Length >= 2)
-                    {
-                        mDict[p[0].Trim()] = p[1].Trim();
+                    if (line.Length == 0) break;
+                    var parts = line.Split('=', 2);
+                    if (parts.Length >= 2) {
+                        m_dict[parts[0].Trim()] = parts[1].Trim();
                     }
                 }
-
-                rd.Close();
             }
         }
 
         public string Get(string key, Dictionary<string, string> overrides)
         {
-            if (overrides.ContainsKey(key))
-            {
+            if (overrides.ContainsKey(key)) {
                 return overrides[key];
-            }
-            else if (mDict.ContainsKey(key))
-            {
+            } else if (m_dict.ContainsKey(key)) {
 #if UNITY_2017_3_OR_NEWER
-                return UnityWebRequest.UnEscapeURL(mDict[key]);
+                return UnityWebRequest.UnEscapeURL(m_dict[key]);
 #else
-                return WWW.UnEscapeURL(mDict[key]);
+                return WWW.UnEscapeURL(m_dict[key]);
 #endif
-            }
-            else
-            {
+            } else {
                 return string.Empty;
             }
         }
 
         public string Get(string key, string defaultValue)
         {
-            if (mDict.ContainsKey(key))
-            {
+            if (m_dict.ContainsKey(key)) {
 #if UNITY_2017_3_OR_NEWER
-                return UnityWebRequest.UnEscapeURL(mDict[key]);
+                return UnityWebRequest.UnEscapeURL(m_dict[key]);
 #else
-                return WWW.UnEscapeURL(mDict[key]);
+                return WWW.UnEscapeURL(m_dict[key]);
 #endif
-            }
-            else
-            {
+            } else {
                 return defaultValue;
             }
         }
@@ -148,12 +118,12 @@ namespace GooglePlayGames.Editor
         public void Set(string key, string val)
         {
 #if UNITY_2017_3_OR_NEWER
-            string escaped = UnityWebRequest.EscapeURL(val);
+            var escaped = UnityWebRequest.EscapeURL(val);
 #else
-            string escaped = WWW.EscapeURL(val);
+            var escaped = WWW.EscapeURL(val);
 #endif
-            mDict[key] = escaped;
-            mDirty = true;
+            m_dict[key] = escaped;
+            m_dirty = true;
         }
 
         public void Set(string key, bool val)
@@ -163,35 +133,28 @@ namespace GooglePlayGames.Editor
 
         public void Save()
         {
-            // See if we are building the plugin, and don't write the settings file
-            string[] args = System.Environment.GetCommandLineArgs();
-            foreach (string a in args)
-            {
-                if (a == "-g.building")
-                {
-                    mDirty = false;
+            var args = System.Environment.GetCommandLineArgs();
+            foreach (var arg in args) {
+                if (arg == "-g.building") {
+                    m_dirty = false;
                     break;
                 }
             }
+            if (!m_dirty) return;
 
-            if (!mDirty)
-            {
-                return;
+            using (var sw = new StreamWriter(m_file, false)) {
+                foreach (var key in m_dict.Keys) {
+                    sw.WriteLine(key + "=" + m_dict[key]);
+                }
             }
-
-            StreamWriter wr = new StreamWriter(mFile, false);
-            foreach (string key in mDict.Keys)
-            {
-                wr.WriteLine(key + "=" + mDict[key]);
-            }
-
-            wr.Close();
-            mDirty = false;
+            m_dirty = false;
         }
 
         public static void Reload()
         {
-            sInstance = new GPGSProjectSettings();
+            s_instance = new GPGSProjectSettings();
         }
+
     }
+
 }
